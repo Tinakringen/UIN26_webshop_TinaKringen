2020d7ad6babf0e591de85217cbe70b55a3c3e0b
Tinakringen <tinakringen98@gmail.com>
2026-02-16 12:28:04 +0100

    Blir kjent med routing i react
    Opprettet komponenter og koblet alle sammen opp

---
+++ Routing/src/components/CategoryLayout.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function CategoryLayout() {
+    return (
+        <>
+        <nav>
+            <Link to="sko">Sko</Link>
+            <Link to="bukse">Bukse</Link>
+            <Link to="topp">Topp</Link>
+            <Link to="kjole">Kjole</Link>
+            <Link to="skjorte">Skjorte</Link>
+            <Link to="bh">BH</Link>
+        </nav>
+        <Outlet />
+        </>
+    )
+}
