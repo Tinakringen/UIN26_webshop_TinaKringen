57b05f378d8d0fcd0141c09119fea852bf4743ab
Tinakringen <tinakringen98@gmail.com>
2026-02-23 14:58:40 +0100

    Lagt inn bildemappe, opprettet CategoryCard
    Oppdatert kode i Category og Categories

--- Routing/src/components/Category.jsx
+++ Routing/src/components/Category.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useOutletContext, useParams } from "react-router-dom"
 
 export default function Category() {
-
+    const {apiEndpoint, defaultApiUrl} = useOutletContext()
     const [apiData, setApiData] = useState([])
+    const [spritesImg, setSpritesImg] = useState([])
     const {slug} = useParams()
 
+    console.log("Denne kommer fra Category", apiEndpoint)
+
     const getSingleData = async()=>{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${slug}`)
+        const response = await fetch(apiEndpoint ? apiEndpoint : defaultApiUrl + slug)
         const data = await response.json()
         setApiData(data)
     }
 
-    console.log(apiData)
+    console.log("Cat", apiData, apiEndpoint)
+
+    console.log("Key values: ", Object.keys(apiData?.sprites))
 
     useEffect( ()=>{
         getSingleData()
-    }, [slug])
+        setSpritesImg(Object.keys(apiData?.sprites))
+    }, [slug, apiEndpoint])
+
+    console.log("Mine bilder", spritesImg)
 
     return (
         <main>
             <h1>{apiData?.name}</h1>
             <section>
                 <h2>Bilder</h2>
+                {spritesImg?.map((item) => <img src={apiData?.sprites?.item} alt={apiData?.name})
                 <img src={apiData?.sprites?.front_default} alt={apiData?.name} />
             </section>
         </main>
--- Routing/src/components/CategoryLayout.jsx
+++ Routing/src/components/CategoryLayout.jsx
@@ -1,32 +1,37 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'
 
 export default function CategoryLayout() {
 
+    const {slug} = useParams()
     const [apiData, setApiData] = useState([])
     const [apiEndpoint, setApiEndpoint] = useState()
 
+    const defaultApiUrl = 'https://pokeapi.co/api/v2/' 
+
     const getData = async()=>{
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10')
+        const response = await fetch(defaultApiUrl)
         const data = await response.json()
-        setApiData(data.results)
-        console.log(data)
+        const {type, pokemon, item} = data
+        
+        setApiData({type, pokemon, item})
     }
 
-    console.log(apiData)
-    console.log(apiEndpoint)
+    console.log("Sjekk", Object.keys(apiData))
+    console.log("Denne kommer fra Layout", apiEndpoint)
 
     useEffect( ()=>{
     getData()
-    }, [])
+    }, [slug])
 
     return (
         <>
         <nav className="main-nav">
-            {apiData?.map((item) => <Link key={item.name + '-xt'} to={item.name} 
-            onClick={()=> setApiEndpoint(item.url)}>{item.name}</Link>)}
+            {Object.keys(apiData)?.map((item) => <Link key={item+'-random'} to={item} onClick={() => setApiEndpoint(defaultApiUrl + item)}>{item}</Link>)}
+            {/*apiData?.map((item) => <Link key={item.name + '-xt'} to={item.name} 
+            onClick={()=> setApiEndpoint(item.url)}>{item.name}</Link>)*/}
         </nav>
-        <Outlet />
+        <Outlet context={{apiEndpoint, defaultApiUrl, setApiEndpoint}}/>
         </>
     )
 }
