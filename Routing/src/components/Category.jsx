f1176dfa3cac25b960c44ed260e429034b9250b1
Tinakringen <tinakringen98@gmail.com>
2026-03-17 12:32:05 +0100

    La til eksamensoving.
    Hadde i oppgave å prøve å løse bugs for siden.

--- Routing/src/components/Category.jsx
+++ Routing/src/components/Category.jsx
@@ -4,35 +4,33 @@ import { useOutletContext, useParams } from "react-router-dom"
 export default function Category() {
     const {apiEndpoint, defaultApiUrl} = useOutletContext()
     const [apiData, setApiData] = useState([])
-    const [spritesImg, setSpritesImg] = useState([])
-    const {slug} = useParams()
+    const spritesImg = apiData?.sprites 
+    ? Object.keys(apiData.sprites) 
+    : []
+    
+    const { slug, cat } = useParams()
 
     console.log("Denne kommer fra Category", apiEndpoint)
 
     const getSingleData = async()=>{
-        const response = await fetch(apiEndpoint ? apiEndpoint : defaultApiUrl + slug)
+        const response = await fetch(apiEndpoint ? apiEndpoint : defaultApiUrl + slug + "/" + cat)
         const data = await response.json()
         setApiData(data)
     }
 
-    console.log("Cat", apiData, apiEndpoint)
-
-    console.log("Key values: ", Object.keys(apiData?.sprites))
-
     useEffect( ()=>{
         getSingleData()
-        setSpritesImg(Object.keys(apiData?.sprites))
-    }, [slug, apiEndpoint])
-
-    console.log("Mine bilder", spritesImg)
+    }, [cat, apiEndpoint])
 
     return (
         <main>
             <h1>{apiData?.name}</h1>
             <section>
                 <h2>Bilder</h2>
-                {spritesImg?.map((item) => <img src={apiData?.sprites?.item} alt={apiData?.name})
-                <img src={apiData?.sprites?.front_default} alt={apiData?.name} />
+                {spritesImg?.map((item) => (
+                    apiData?.sprites?.[item] ? <img key={item} src={apiData?.sprites?.[item]} alt={apiData?.name} /> 
+                    : null
+                ))}
             </section>
         </main>
     )
